07bb7d5dfe28e75feeb4de3ef3aae961e7377c64
Ilya <iam.goryn@gmail.com>
2025-03-17 22:07:43 +0300

    added a new buttons

--- mta.py
+++ mta.py
@@ -8,9 +8,22 @@ class MainApp(tk.Tk):
 
         self.title("MTA")
         self.iconbitmap("images/mta.ico")
-        self.geometry("500x600")
+        self.geometry("200x300")
 
+        self.label = ttk.Label(text='Выберите тест', font=('Arial', 20))
+        self.label.pack(anchor='center')
 
+        self.btn1 = ttk.Button(text='Тест GPU')
+        self.btn1.pack(anchor='center')
+        self.btn2 = ttk.Button(text='Тест CPU')
+        self.btn2.pack(anchor='center')
+        self.btn3 = ttk.Button(text='Тест RAM')
+        self.btn3.pack(anchor='center')
+        self.btn4 = ttk.Button(text='Тест ROM')
+        self.btn4.pack(anchor='center')
+
+        self.btn5 = ttk.Button(text='Собрать отчёт')
+        self.btn5.pack(anchor='center')
 
 if __name__ == '__main__':
     app = MainApp()
