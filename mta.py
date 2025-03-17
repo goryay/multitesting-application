3356880fed137e4cd29fd71bb261b5d19af3c8ab
Ilya <iam.goryn@gmail.com>
2025-03-17 20:54:24 +0300

    .\mta.py refactoring

--- mta.py
+++ mta.py
@@ -1,8 +1,17 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
-root = Tk()
-root.title("MTA")
-root.iconbitmap("images/mta.ico")
-root.geometry("500x600")
-root.mainloop()
+
+class MainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("MTA")
+        self.iconbitmap("images/mta.ico")
+        self.geometry("500x600")
+
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.mainloop()
