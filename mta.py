ce07d0d656c8c90cd7a7b7273c3ff9a569cbee46
Ilya <iam.goryn@gmail.com>
2025-03-21 14:43:41 +0300

    a class with programs and their functions

--- mta.py
+++ mta.py
@@ -7,7 +7,68 @@ from PIL import ImageGrab
 import GPUtil
 
 
+class BaseTester:
+    def __init__(self, name):
+        self.name = name
+        self.process = None
 
+    def start(self):
+        raise NotImplementedError("Этот метод должен быть переопределен подклассами")
+
+    def stop(self):
+        if self.process:
+            self.process.terminate()
+            self.process = None
+
+    def take_screenshot(self, filename):
+        screenshot = ImageGrab.grab()
+        screenshot.save(filename)
+
+
+class AidaBusinessTester(BaseTester):
+    def __init__(self, name, gpu_enabled):
+        super().__init__(name)
+        self.gpu_enabled = gpu_enabled
+
+    def start(self):
+        command = ["aida64.exe", "--stress-cpu", "--stress-fpu", "--stress-cache", "--stress-memory"]
+        if self.gpu_enabled:
+            command.append("--stress-gpu")
+        self.process = subprocess.Popen(command)
+
+
+class FurmarkTester(BaseTester):
+    def start(self):
+        self.process = subprocess.Popen(["furmark.exe"])
+
+
+class FioTester(BaseTester):
+    def start(self):
+        self.process = subprocess.Popen(["fio", "--filename=test.fio", "--size=1G", "--rw=write"])
+
+
+class CrystalDiskInfoTester(BaseTester):
+    def start(self):
+        self.process = subprocess.Popen(["CrystalDiskInfo.exe", "--smart"])
+
+
+class TestManager:
+    def __init__(self):
+        self.testers = []
+        self.test_time = 12 * 60 * 60
+
+    def add_tester(self, tester):
+        self.testers.append(tester)
+
+    def start_all(self):
+        for tester in self.testers:
+            tester.start()
+        threading.Timer(self.test_time, self.stop_all).start()
+
+    def stop_all(self):
+        for tester in self.testers:
+            tester.take_screenshot(f"{tester.name}_screenshot.png")
+            tester.stop()
 
 
 class App:
@@ -21,6 +82,7 @@ class App:
         except tk.TclError:
             print("Картинка не найдена.")
 
+        self.manager = TestManager()
 
         # Инициализация переменных для чекбоксов
         self.aida_var = tk.BooleanVar()
